199d62999a98b20a1f254b36fd0c79804f6ba8ff
Sat Ram <sathyanasa@gmail.com>
2017-05-25 18:33:02 -0700

    added code for articles

---
+++ index.js
@@ -0,0 +1,27 @@
+var LikeButton = document.getElementById("like-button");
+var DisLikeButton = document.getElementById("dislike-button");
+
+//LikeButton.addEventListener('click', inc);
+//DisLikeButton.addEventListener('click', dec);
+
+
+var article = document.getElementsByClassName('post');
+var allnumber = document.getElementsByClassName('post-number');
+var test = document.getElementsByTagName("input");
+
+
+var btn = document.getElementsByTagName("button");
+for(var i = 0; i < btn.length; i++){
+  if(i % 2 == 0){
+    console.log(Math.floor(i/2));
+    btn[i].addEventListener('click', function(){test[Math.floor(i/2)] = Number(test[Math.floor(i/2)]) + 1;});
+    }
+  else{
+    btn[i].addEventListener('click', dec);
+  }
+}
+
+
+function dec(){
+  test[0].value = Number(test[0].value) - 1;
+}
