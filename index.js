606a2b1b4403c2d5836ee4a5a0ed101fc0140d06
Sat Ram <sathyanasa@gmail.com>
2017-05-25 19:20:55 -0700

    background color changed

--- index.js
+++ index.js
@@ -12,14 +12,19 @@ var test = document.getElementsByTagName("input");
 var btn = document.getElementsByTagName("button");
 for(var i = 0; i < btn.length; i++){
   if(i % 2 == 0){
-    console.log(Math.floor(i/2));
-    btn[i].addEventListener('click', function(){test[Math.floor(i/2)] = Number(test[Math.floor(i/2)]) + 1;});
+    //console.log(Math.floor(i/2));
+    //btn[i].addEventListener('click', function(){test[Math.floor(i/2)] = Number(test[Math.floor(i/2)]) + 1;});
+    btn[i].addEventListener('click', inc);
+    console.log("hi");
     }
   else{
     btn[i].addEventListener('click', dec);
   }
 }
 
+function inc(){
+  test[0].value = Number(test[0].value) + 1;
+}
 
 function dec(){
   test[0].value = Number(test[0].value) - 1;
