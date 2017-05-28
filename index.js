0479a3616aa1ecc7604add13bb78896266e5985e
Sat Ram <sathyanasa@gmail.com>
2017-05-28 16:20:02 -0700

    Like/Dislike buttons work for each post!!!

--- index.js
+++ index.js
@@ -4,28 +4,37 @@ var DisLikeButton = document.getElementById("dislike-button");
 //LikeButton.addEventListener('click', inc);
 //DisLikeButton.addEventListener('click', dec);
 
-
 var article = document.getElementsByClassName('post');
-var allnumber = document.getElementsByClassName('post-number');
-var test = document.getElementsByTagName("input");
+var input = document.getElementsByTagName("input");
 
 var btn = document.getElementsByTagName("button");
 for(var i = 0; i < btn.length; i++){
   if(i % 2 == 0){
-    //console.log(Math.floor(i/2));
-    //btn[i].addEventListener('click', function(){test[Math.floor(i/2)] = Number(test[Math.floor(i/2)]) + 1;});
-    btn[i].addEventListener('click', inc);
-    console.log("hi");
-    }
+    (function(index){
+      btn[i].onclick = function(){
+          inc(index);
+      }
+    })(i);
+  }
   else{
-    btn[i].addEventListener('click', dec);
+    (function(index){
+      btn[i].onclick = function(){
+          dec(index);
+      }
+    })(i);
   }
 }
 
-function inc(){
-  test[0].value = Number(test[0].value) + 1;
+function inc(index){
+  index = Math.floor(index/2);
+  input[index].value = Number(input[index].value) + 1;
 }
 
-function dec(){
-  test[0].value = Number(test[0].value) - 1;
+function dec(index){
+  index = Math.floor(index/2);
+  input[index].value = Number(input[index].value) - 1;
 }
+
+/*for(var i = 0; i < input.length; i++){
+  console.log(input[i].value);
+}*/
